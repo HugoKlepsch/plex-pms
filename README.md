652f4ded774e5d9ebca3c7cb2a3f881dbf50f10f
Hugo Klepsch <hugo.klepsch@gmail.com>
2025-06-21 21:03:14 +0000

    plex 2

--- README.md
+++ README.md
@@ -1,4 +1,64 @@
-# Samba credentials 
+# Plex 2 setup
+
+---
+
+Plex 2 is the development name for the next-generation depoyment of
+my home media server and its supporting systems. It is so named because
+I already have an in-use deployment of plex, which I intend to maintain
+during development.
+
+# High level design
+
+## Components
+
+* Plex - Media server application, content metadata.
+* Lidarr - Music collection auto-downloader
+* Radarr - Movie collection auto-downloader
+* Readarr - Book collection auto-downloader
+* Sonarr - TV collection auto-downloader
+* Bazarr - Subtitle auto-downloader
+* Prowlarr - Central indexer, supporting Lidarr, Radar, Readarr, Sonarr
+* Oversearr - Media requester UI
+* TBD - Bittorrent client
+
+Components are run as docker containers, started with docker-compose,
+service lifecycle managed by Systemd unit files. Configuration and secrets
+are stored in `.env` files.
+
+## How they work together
+
+Movie example:
+
+1. User makes a request in Overseerr for a movie.
+2. Overseerr tells Radarr to add the movie.
+3. Radarr asks Prowlarr to search for it across configured indexers.
+4. Prowlarr finds it and returns the best match.
+5. Radarr sends it to torrent client (qBittorrent, NZBGet, etc.).
+6. Once downloaded, Radarr renames and moves it into media library.
+7. Plex picks it up and it becomes available for viewing.
+
+## Permissions
+
+* Plex will run as `plex` and \*arr apps will run as `arr`.
+* Both `plex` and `arr` are in `plexapp` group.
+* Server management user is also in `plexapp` group.
+
+## Storage
+
+* All data is stored on my Synology NAS, mounted via Samba.
+* Application configs are not on the NAS-mounted drive, because they contain
+SQLite DBs, and don't tend to work well with NAS mounts because they don't 
+support file locking properly.
+* Mounted as `plex:plexapp`
+* All data is on one filesystem so that hardlinks can be used to enable
+  atomic moves and deduplication.
+* During development, 
+
+# Details
+
+## Data mounts
+
+### Samba credentials 
 
 Create a credentials file here:
 
@@ -21,30 +81,17 @@ Protect it:
 sudo chmod 600 /etc/samba/creds_plex_data
 ```
 
-# Systemd mount using CIFS
+### Systemd mount using CIFS
 
-File, where `home-user-plex-plex_data_mnt` refers to `/home/user/plex/plex_data_mnt`:
 
-```bash
-sudo vim /etc/systemd/system/home-user-plex-plex_data_mnt.mount
-```
+The `create-systemd-service.sh` script will generate a set of systemd units, 
+one of which mounts the samba share. For me, I want to mount it to `/home/user/plex2/plex_data_mnt`,
+so it creates a `home-user-plex2-plex_data_mnt.mount` unit. This is then installed into `/etc/systemd/system/`.
 
-Contents:
+# Progress
 
-```
-[Unit]
-Description=Mount SMB Share
-After=network-online.target
-Wants=network-online.target
-
-[Mount]
-What=//<hostname or ip>/<share>
-Where=/path/to/where/it/goes
-Type=cifs
-# Get UID and GID using `id` for the user to mount as
-Options=credentials=/etc/samba/creds_plex_data,uid=1000,gid=1000,iocharset=utf8,nofail
-TimeoutSec=30
-
-[Install]
-WantedBy=multi-user.target
-```
+* plex2 NAS mounting has migrated from sshfs -> SMB [DONE]
+* plex2 users `plex` and `arr` as well as `plexapp` group created [DONE]
+* plex2 running under `plex:plexapp`, replacing old plex deployment [DONE]
+* NAS mounted as `nobody:plexapp` [WON'T DO: plex doesn't like it...]
+* Arrs... [TODO]
--- create-systemd-service.sh
+++ create-systemd-service.sh
@@ -2,15 +2,62 @@
 # Create a systemd service that autostarts & manages a docker-compose instance in the current directory
 # by Uli Köhler - https://techoverflow.net
 # Licensed as CC0 1.0 Universal
+# Modified by Hugo Klepsch
+
+set -euo pipefail
+
 SERVICENAME=$(basename $(pwd))
 
-echo "Creating systemd service... /etc/systemd/system/${SERVICENAME}.service"
+# Load variables
+ENV_FILE=".env"
+
+if [[ ! -f "$ENV_FILE" ]]; then
+  echo "Error: $ENV_FILE file not found." >&2
+  exit 1
+fi
+
+# Use 'set -a' to export all sourced variables to the environment
+set -a
+if ! source "$ENV_FILE"; then
+  echo "Error: Failed to source $ENV_FILE." >&2
+  exit 1
+fi
+set +a
+echo "$ENV_FILE loaded successfully."
+
+# Generate the systemd mount unit name
+path="$(pwd)/${mount_dir}"
+
+# Strip leading slash, replace slashes with dashes and append ".mount"
+mount_unit_name="${path#/}"
+mount_unit_name="${mount_unit_name//\//-}.mount"
+
+echo "Creating systemd samba mount... ${mount_unit_name}"
+# Create systemd mount file
+cat >"${mount_unit_name}" <<EOF
+[Unit]
+Description=Plex SMB Share
+After=network-online.target
+Wants=network-online.target
+
+[Mount]
+What=//${smb_host}/${smb_drive}
+Where=${path}
+Type=cifs
+Options=credentials=/etc/samba/creds_plex_data,uid=${mount_user},gid=${mount_group},file_mode=0775,dir_mode=0775,iocharset=utf8,nofail
+TimeoutSec=30
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Creating systemd service... ${SERVICENAME}.service"
 # Create systemd service file
-cat >$SERVICENAME.service <<EOF
+cat >"${SERVICENAME}.service" <<EOF
 [Unit]
 Description=$SERVICENAME
-After=home-user-plex-plex_data_mnt.mount docker.service
-Requires=home-user-plex-plex_data_mnt.mount docker.service
+After=${mount_unit_name} docker.service
+Requires=${mount_unit_name} docker.service
 
 [Service]
 RestartSec=10
@@ -19,21 +66,33 @@ User=root
 Group=docker
 WorkingDirectory=$(pwd)
 # Shutdown container (if running) when unit is started
-ExecStartPre=/bin/bash -c ". env.bash; $(which docker-compose) down"
+ExecStartPre=/bin/bash -c ". .env; $(which docker-compose) down"
 # Start container when unit is started
-ExecStart=/bin/bash -c ". env.bash; $(which docker-compose) up"
+ExecStart=/bin/bash -c ". .env; $(which docker-compose) up"
 # Stop container when unit is stopped
-ExecStop=/bin/bash -c ". env.bash; $(which docker-compose) down"
+ExecStop=/bin/bash -c ". .env; $(which docker-compose) down"
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-sudo cp "${SERVICENAME}.service" "/etc/systemd/system/$SERVICENAME.service"
+if [[ "${INSTALL:-false}" == "true" ]]; then
+	echo "Installing systemd samba mount... /etc/systemd/system/${mount_unit_name}"
+	sudo cp "${mount_unit_name}" "/etc/systemd/system/${mount_unit_name}"
 
-echo "Enabling & starting $SERVICENAME"
-# Autostart systemd service
-sudo systemctl enable $SERVICENAME.service
-# Start systemd service now
-sudo systemctl start $SERVICENAME.service
+	echo "Installing systemd service... /etc/systemd/system/$SERVICENAME.service"
+	sudo cp "${SERVICENAME}.service" "/etc/systemd/system/$SERVICENAME.service"
 
+	sudo systemctl daemon-reload
+
+	echo "Enabling & starting ${mount_unit_name} and $SERVICENAME"
+	# Start systemd units on startup (and right now)
+	sudo systemctl enable --now "${mount_unit_name}"
+	sudo systemctl enable --now "${SERVICENAME}.service"
+	exit 0
+else
+	echo "Run with INSTALL=true ./create... to install"
+	exit 0
+fi
+
+exit 0
